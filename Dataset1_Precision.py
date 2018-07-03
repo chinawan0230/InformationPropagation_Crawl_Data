3b6989ac11912d08f37798be473f65523483ab99
chinawan0230 <37644884+chinawan0230@users.noreply.github.com>
2018-07-03 17:29:54 +0800

    Add files via upload

---
+++ Dataset1_Precision.py
@@ -0,0 +1,28 @@
+# encoding=utf-8
+import matplotlib.pyplot as plt
+from pylab import *                                 #支持中文
+names = ['1', '2', '3', '4', '5', '6', '7']
+x = range(len(names))
+y2 = [0.055, 0.047, 0.043, 0.042, 0.041, 0.040, 0.043]
+y = [0.073, 0.070, 0.072, 0.068, 0.071, 0.077, 0.079]
+y1 = [0.062, 0.060, 0.062, 0.057, 0.065, 0.061, 0.063]
+y3 = [0.083, 0.075, 0.080, 0.076, 0.074, 0.079, 0.083]
+#plt.plot(x, y, 'ro-')
+#plt.plot(x, y1, 'bo-')
+#pl.xlim(-1, 11)  # 限定横轴的范围
+#pl.ylim(-1, 110)  # 限定纵轴的范围
+plt.plot(x, y2, marker='^', ms=10, label='CT + General threshold model')
+plt.plot(x, y, marker='s', label='GT (Diffusion cascades)')
+plt.plot(x, y1, marker='*', ms=10, label='GT (Page rank)')
+
+plt.plot(x, y3, marker='.', ms=10, label='Our method')
+plt.figure(1)
+plt.legend()  # 让图例生效
+plt.xticks(x, names)
+plt.margins(0.05)
+plt.subplots_adjust(bottom=0.15)
+plt.xlabel("Time step (3 days)")  # X轴标签
+plt.ylabel("Precision")  # Y轴标签
+# plt.title("A simple plot")  # 标题
+
+plt.show()
